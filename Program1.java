24c19b4ab76b8bbc250ae5b8c4250780f9d7fc94
Sawfeeq Mohammed <sawfeeqrox@gmail.com>
2021-09-16 23:29:42 +0530

    project 1 testing

---
+++ Program1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Program1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        int age=sc.nextInt();
+        System.out.println("Name is: "+name);
+        System.out.println("Age is: "+age);
+
+
+
+
+    }
+}
